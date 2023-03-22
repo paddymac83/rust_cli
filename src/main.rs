68c38ba2fc1c144103696c7c9e2546cfedf6bee0
root <root@LAPTOP-4KHFE4JU.localdomain>
2023-03-22 21:59:49 +0000

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,19 @@
+use std::env;
+use std::fs;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();  // convert itr to collection
+    // dbg!(args);   // print args, remove to add refs below as dbg! owns args
+
+    // save refs in variables, [0] is the program name
+    let query = &args[1];
+    let file_path = &args[2];
+    
+    println!("Searching for {}", query);
+    println!("In file {}", file_path);
+
+    // return std::io::Result<String> from file
+    let contents = fs::read_to_string(file_path).expect("Should have a file to read..");
+
+    println!("With text:\n{contents}")
+}
