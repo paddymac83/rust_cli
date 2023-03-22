ac5d924c6f6821a60426691edc0565d421e3e572
root <root@LAPTOP-4KHFE4JU.localdomain>
2023-03-22 22:10:48 +0000

    Added update to main.rs module

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,7 @@ fn main() {
     // dbg!(args);   // print args, remove to add refs below as dbg! owns args
 
     // save refs in variables, [0] is the program name
-    let query = &args[1];
-    let file_path = &args[2];
-    
+    let (query, file_path) = parse_config(&args);
     println!("Searching for {}", query);
     println!("In file {}", file_path);
 
@@ -16,4 +14,12 @@ fn main() {
     let contents = fs::read_to_string(file_path).expect("Should have a file to read..");
 
     println!("With text:\n{contents}")
+}
+
+fn parse_config(args: &[String]) -> (&str, &str) {
+    let query = &args[1];
+    let file_path = &args[2];
+
+    (query, file_path)
+
 }
