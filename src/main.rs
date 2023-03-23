c7a9dff7cd83568c91f131338b398c051e8fe52f
root <root@LAPTOP-4KHFE4JU.localdomain>
2023-03-23 21:46:57 +0000

    Updated the CLI with modular code

--- src/main.rs
+++ src/main.rs
@@ -5,21 +5,30 @@ fn main() {
     let args: Vec<String> = env::args().collect();  // convert itr to collection
     // dbg!(args);   // print args, remove to add refs below as dbg! owns args
 
+    let config = Config::new(&args);  // constructor
     // save refs in variables, [0] is the program name
-    let (query, file_path) = parse_config(&args);
-    println!("Searching for {}", query);
-    println!("In file {}", file_path);
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.file_path);
 
     // return std::io::Result<String> from file
-    let contents = fs::read_to_string(file_path).expect("Should have a file to read..");
+    let contents = fs::read_to_string(config.file_path).expect("Should have a file to read..");
 
     println!("With text:\n{contents}")
 }
 
-fn parse_config(args: &[String]) -> (&str, &str) {
-    let query = &args[1];
-    let file_path = &args[2];
+struct Config {
+    query: String,
+    file_path: String,
+}
 
-    (query, file_path)
-
+impl Config {
+    fn new(args: &[String]) -> Config {
+        if args.len() < 3 {
+            panic!("Not enough arguments");
+        }
+        let query = args[1].clone();                  // this is a clone as args is owned in main()
+        let file_path = args[2].clone();
+    
+        Config { query, file_path }
+    }
 }
