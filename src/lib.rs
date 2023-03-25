78d1a9cb3dc5ee4dd83c9e24b1b7317ea380444b
root <root@LAPTOP-4KHFE4JU.localdomain>
2023-03-25 21:44:28 +0000

    Added unit tests

--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,9 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     // return std::io::Result<String> from file
     let contents = fs::read_to_string(config.file_path)?;
 
-    println!("With text:\n{contents}");
+    for line in search(&config.query, &contents) {  // returns and prints a Vec
+        println!(" {line} ");
+    }
 
     Ok(())
 
@@ -26,5 +28,32 @@ impl Config {
         let file_path = args[2].clone();
     
         Ok(Config { query, file_path })
+    }
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {  // line returned references the contents and is valid as long as contents is
+    let mut results = Vec::new();
+    for line in contents.lines() {  // returns an iterator
+            if line.contains(query) {
+                results.push(line);
+            }
+    }
+    results
+}
+
+// add tests mod in lib.rs
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
     }
 }
